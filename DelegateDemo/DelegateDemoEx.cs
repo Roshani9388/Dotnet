c5439c3a643b74360c24df91cae8cfb87369de5a
Roshani9388 <131379079+Roshani9388@users.noreply.github.com>
2023-07-18 10:13:04 +0530

    Add files via upload

---
+++ DelegateDemo/DelegateDemoEx.cs
@@ -0,0 +1,20 @@
+﻿ namespace DelegateDemo;
+
+public class DelegateDemoEx
+{
+    
+     
+    public void Add(int n1,int n2){
+        int sum=n1+n2;
+        Console.WriteLine("Summm == " +sum);
+
+    }
+
+ public void Sub(int n1,int n2){
+        int sub=n1-n2;
+        Console.WriteLine("Sub == " +sub);
+    }
+
+
+}
+ 
